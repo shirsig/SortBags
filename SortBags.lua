0aba4fb3d84c043af65654d59f4a696c3c1fb180
Simon Hirsig <simon.hirsig@gmail.com>
2021-10-10 17:02:56 +0200

    classic fresh update

--- SortBags.lua
+++ SortBags.lua
@@ -43,6 +43,14 @@ local function set(...)
 	return t
 end
 
+local function arrayToSet(array)
+	local t = {}
+	for i = 1, #array do
+		t[array[i]] = true
+	end
+	return t
+end
+
 local function union(...)
 	local t = {}
 	local n = select('#', ...)
@@ -54,104 +62,63 @@ local function union(...)
 	return t
 end
 
-local MOUNTS = set(
-	-- rams
-	5864, 5872, 5873, 18785, 18786, 18787, 18244, 19030, 13328, 13329,
-	-- horses
-	2411, 2414, 5655, 5656, 18778, 18776, 18777, 18241, 12353, 12354,
-	-- sabers
-	8629, 8631, 8632, 18766, 18767, 18902, 18242, 13086, 19902, 12302, 12303, 8628, 12326,
-	-- mechanostriders
-	8563, 8595, 13321, 13322, 18772, 18773, 18774, 18243, 13326, 13327,
-	-- kodos
-	15277, 15290, 18793, 18794, 18795, 18247, 15292, 15293,
-	-- wolves
-	1132, 5665, 5668, 18796, 18797, 18798, 18245, 12330, 12351,
-	-- raptors
-	8588, 8591, 8592, 18788, 18789, 18790, 18246, 19872, 8586, 13317,
-	-- undead horses
-	13331, 13332, 13333, 13334, 18791, 18248, 13335,
-	-- qiraji battle tanks
-	21218, 21321, 21323, 21324, 21176
-)
+local SPECIAL = set(5462, 13347, 11511, 38233, 9173)
 
-local SPECIAL = set(5462, 17696, 17117, 13347, 13289, 11511)
+local KEYS = set(9240, 11511, 17191, 13544, 12324, 16309, 12384, 20402)
 
-local KEYS = set(9240, 17191, 13544, 12324, 16309, 12384, 20402)
-
-local TOOLS = set(7005, 12709, 19727, 5956, 2901, 6219, 10498, 6218, 6339, 11130, 11145, 16207, 9149, 15846, 6256, 6365, 6367)
-
-local ENCHANTING_MATERIALS = set(
-	-- dust
-	10940, 11083, 11137, 11176, 16204,
-	-- essence
-	10938, 10939, 10998, 11082, 11134, 11135, 11174, 11175, 16202, 16203,
-	-- shard
-	10978, 11084, 11138, 11139, 11177, 11178, 14343, 14344,
-	-- crystal
-	20725
-)
-
-local HERBS = set(765, 785, 2447, 2449, 2450, 2452, 2453, 3355, 3356, 3357, 3358, 3369, 3818, 3819, 3820, 3821, 4625, 8153, 8831, 8836, 8838, 8839, 8845, 8846, 13463, 13464, 13465, 13466, 13467, 13468)
-
-local SEEDS = set(17034, 17035, 17036, 17037, 17038)
+local TOOLS = set(6218, 6339, 11130, 11145, 16207, 22461, 22462, 22463, 5060, 7005, 12709, 19727, 5956, 2901, 6219, 10498, 9149, 15846, 6256, 6365, 6366, 6367, 12225, 19022, 25978, 19970, 20815, 20824, 25978)
 
 local CLASSES = {
-	-- arrow
-	{
-		containers = {2101, 5439, 7278, 11362, 3573, 3605, 7371, 8217, 2662, 19319, 18714},
-		items = set(2512, 2515, 3030, 3464, 9399, 11285, 12654, 18042, 19316),
-	},
-	-- bullet
-	{
-		containers = {2102, 5441, 7279, 11363, 3574, 3604, 7372, 8218, 2663, 19320},
-		items = set(2516, 2519, 3033, 3465, 4960, 5568, 8067, 8068, 8069, 10512, 10513, 11284, 11630, 13377, 15997, 19317),
-	},
 	-- soul
 	{
 		containers = {22243, 22244, 21340, 21341, 21342},
 		items = set(6265),
 	},
+	-- arrow
+	{
+		containers = {2101, 5439, 7278, 11362, 3573, 3605, 7371, 8217, 2662, 19319, 18714},
+		items = set(2512, 2514, 2515, 3029, 3030, 3031, 3464, 9399, 10579, 11285, 12654, 18042, 19316, 24412, 24417, 28053, 28056, 30319, 30611, 31737, 31949, 32760, 33803, 34581),
+	},
+	-- bullet
+	{
+		containers = {2102, 5441, 7279, 11363, 3574, 3604, 7372, 8218, 2663, 19320},
+		items = set(2516, 2519, 3033, 3465, 4960, 5568, 8067, 8068, 8069, 10512, 10513, 11284, 11630, 13377, 15997, 19317, 23772, 23773, 28060, 28061, 30612, 31735, 32761, 32882, 32883, 34582),
+	},
 	-- ench
 	{
 		containers = {22246, 22248, 22249},
-		items = union(
-			ENCHANTING_MATERIALS,
-			-- rods
-			set(6218, 6339, 11130, 11145, 16207)
-		),
+		items = arrayToSet({6218, 6222, 6339, 6342, 6343, 6344, 6345, 6346, 6347, 6348, 6349, 6375, 6376, 6377, 10938, 10939, 10940, 10978, 10998, 11038, 11039, 11081, 11082, 11083, 11084, 11098, 11101, 11130, 11134, 11135, 11137, 11138, 11139, 11145, 11150, 11151, 11152, 11163, 11164, 11165, 11166, 11167, 11168, 11174, 11175, 11176, 11177, 11178, 11202, 11203, 11204, 11205, 11206, 11207, 11208, 11223, 11224, 11225, 11226, 11813, 14343, 14344, 16202, 16203, 16204, 16207, 16214, 16215, 16216, 16217, 16218, 16219, 16220, 16221, 16222, 16223, 16224, 16242, 16243, 16244, 16245, 16246, 16247, 16248, 16249, 16250, 16251, 16252, 16253, 16254, 16255, 17725, 18259, 18260, 19444, 19445, 19446, 19447, 19448, 19449, 20725, 20726, 20727, 20728, 20729, 20730, 20731, 20732, 20733, 20734, 20735, 20736, 20752, 20753, 20754, 20755, 20756, 20757, 20758, 22392, 22445, 22446, 22447, 22448, 22449, 22450, 22461, 22462, 22463, 22530, 22531, 22532, 22533, 22534, 22535, 22536, 22537, 22538, 22539, 22540, 22541, 22542, 22543, 22544, 22545, 22546, 22547, 22548, 22551, 22552, 22553, 22554, 22555, 22556, 22557, 22558, 22559, 22560, 22561, 22562, 22563, 22564, 22565, 24000, 24003, 25848, 25849, 28270, 28271, 28272, 28273, 28274, 28276, 28277, 28279, 28280, 28281, 28282, 33148, 33149, 33150, 33151, 33152, 33153, 33165, 33307, 34872, 35297, 35298, 35299, 35498, 35500, 35756, 186683, 7081, 12810, 7068, 7972, 12808, 7067, 7075, 7076, 7077, 7078, 7080, 7082, 12803, 21885, 22451, 22456, 22457, 22572, 22576, 22577, 22578, 23571, 23572, 21886, 22575, 21884, 22452, 22573, 22574}),
 	},
 	-- herb
 	{
 		containers = {22250, 22251, 22252},
-		items = union(HERBS, SEEDS)
+		items = arrayToSet({765, 785, 1401, 2263, 2447, 2449, 2450, 2452, 2453, 3355, 3356, 3357, 3358, 3369, 3818, 3819, 3820, 3821, 4625, 5013, 5056, 5168, 8831, 8836, 8838, 8839, 8845, 8846, 11018, 11020, 11022, 11024, 11040, 11514, 11951, 11952, 13463, 13464, 13465, 13466, 13467, 13468, 16205, 16208, 17034, 17035, 17036, 17037, 17038, 17760, 18297, 19727, 22094, 22147, 22710, 22785, 22786, 22787, 22788, 22789, 22790, 22791, 22792, 22793, 22794, 22795, 22797, 23329, 23501, 23788, 24245, 24246, 24401, 31300, 32468, 34465, 8153, 10286, 19726, 21886, 22575}),
 	},
 }
 
 do
 	local f = CreateFrame'Frame'
-	f:SetScript('OnEvent', function()
-		f:SetScript('OnUpdate', function()
+	local lastUpdate = 0
+	local function updateHandler()
+		if GetTime() - lastUpdate > 1 then
 			for _, container in pairs(BAG_CONTAINERS) do
 				for position = 1, GetContainerNumSlots(container) do
 					SetScanTooltip(container, position)
 				end
 			end
-			f:SetScript('OnUpdate', nil)
-		end)
-	end)
-	f:RegisterEvent'PLAYER_LOGIN'
-end
-
-do
-	local f = CreateFrame'Frame'
-	f:SetScript('OnEvent', function()
-		for _, container in pairs(BANK_BAG_CONTAINERS) do
-			for position = 1, GetContainerNumSlots(container) do
-				SetScanTooltip(container, position)
+			for _, container in pairs(BANK_BAG_CONTAINERS) do
+				for position = 1, GetContainerNumSlots(container) do
+					SetScanTooltip(container, position)
+				end
 			end
+			f:SetScript('OnUpdate', nil)
 		end
+	end
+	f:SetScript('OnEvent', function()
+		lastUpdate = GetTime()
+		f:SetScript('OnUpdate', updateHandler)
 	end)
+	f:RegisterEvent'BAG_UPDATE'
 	f:RegisterEvent'BANKFRAME_OPENED'
 end
 
@@ -213,12 +180,12 @@ function LT(a, b)
 end
 
 function Move(src, dst)
-    local texture, _, srcLocked = GetContainerItemInfo(src.container, src.position)
-    local _, _, dstLocked = GetContainerItemInfo(dst.container, dst.position)
-    
+	local texture, _, srcLocked = GetContainerItemInfo(src.container, src.position)
+	local _, _, dstLocked = GetContainerItemInfo(dst.container, dst.position)
+
 	if texture and not srcLocked and not dstLocked then
 		ClearCursor()
-       	PickupContainerItem(src.container, src.position)
+		PickupContainerItem(src.container, src.position)
 		PickupContainerItem(dst.container, dst.position)
 
 		if src.item == dst.item then
@@ -235,41 +202,41 @@ function Move(src, dst)
 
 		coroutine.yield()
 		return true
-    end
+	end
 end
 
 do
-    local patterns = {}
-    for i = 1, 10 do
-    	local text = gsub(format(ITEM_SPELL_CHARGES, i), '(-?%d+)(.-)|4([^;]-);', function(numberString, gap, numberForms)
-	        local singular, dual, plural
-	        _, _, singular, dual, plural = strfind(numberForms, '(.+):(.+):(.+)');
-	        if not singular then
-	            _, _, singular, plural = strfind(numberForms, '(.+):(.+)')
-	        end
-	        local i = abs(tonumber(numberString))
-	        local numberForm
-	        if i == 1 then
-	            numberForm = singular
-	        elseif i == 2 then
-	            numberForm = dual or plural
-	        else
-	            numberForm = plural
-	        end
-	        return numberString .. gap .. numberForm
-	    end)
-        patterns[text] = i
-    end
+	local patterns = {}
+	for i = 1, 10 do
+		local text = gsub(format(ITEM_SPELL_CHARGES, i), '(-?%d+)(.-)|4([^;]-);', function(numberString, gap, numberForms)
+			local singular, dual, plural
+			_, _, singular, dual, plural = strfind(numberForms, '(.+):(.+):(.+)');
+			if not singular then
+				_, _, singular, plural = strfind(numberForms, '(.+):(.+)')
+			end
+			local i = abs(tonumber(numberString))
+			local numberForm
+			if i == 1 then
+				numberForm = singular
+			elseif i == 2 then
+				numberForm = dual or plural
+			else
+				numberForm = plural
+			end
+			return numberString .. gap .. numberForm
+		end)
+		patterns[text] = i
+	end
 
 	function itemCharges(text)
-        return patterns[text]
+		return patterns[text]
 	end
 end
 
 function TooltipInfo(container, position)
 	SetScanTooltip(container, position)
 
-	local charges, usable, soulbound, quest, conjured
+	local charges, usable, soulbound, quest, conjured, mount
 	for i = 1, SortBagsTooltip:NumLines() do
 		local text = getglobal('SortBagsTooltipTextLeft' .. i):GetText()
 
@@ -284,10 +251,12 @@ function TooltipInfo(container, position)
 			quest = true
 		elseif text == ITEM_CONJURED then
 			conjured = true
+		elseif text == MOUNT then
+			mount = true
 		end
 	end
 
-	return charges or 1, usable, soulbound, quest, conjured
+	return charges or 1, usable, soulbound, quest, conjured, mount
 end
 
 function SetScanTooltip(container, position)
@@ -442,13 +411,13 @@ end
 function ContainerClass(container)
 	if container ~= 0 and container ~= BANK_CONTAINER then
 		local name = GetBagName(container)
-		if name then		
+		if name then
 			for class, info in pairs(CLASSES) do
 				for _, itemID in pairs(info.containers) do
 					if name == GetItemInfo(itemID) then
 						return class
 					end
-				end	
+				end
 			end
 		end
 	end
@@ -457,11 +426,10 @@ end
 function Item(container, position)
 	local link = GetContainerItemLink(container, position)
 	if link then
-		local _, _, itemID, enchantID, suffixID, uniqueID = strfind(link, 'item:(%d+):(%d*):::::(%d*):(%d*)')
+        local _, _, itemID, enchantID, suffixID, uniqueID = strfind(link, 'item:(%d+):(%d*):::::(%d*):(%d*)')
 		itemID = tonumber(itemID)
 		local itemName, _, quality, _, _, _, _, stack, slot, _, sellPrice, classId, subClassId = GetItemInfo('item:' .. itemID)
-		local charges, usable, soulbound, quest, conjured = TooltipInfo(container, position)
-
+		local charges, usable, soulbound, quest, conjured, mount = TooltipInfo(container, position)
 		local sortKey = {}
 
 		-- hearthstone
@@ -469,7 +437,7 @@ function Item(container, position)
 			tinsert(sortKey, 1)
 
 		-- mounts
-		elseif MOUNTS[itemID] then
+		elseif mount then
 			tinsert(sortKey, 2)
 
 		-- special items
@@ -486,11 +454,11 @@ function Item(container, position)
 
 		-- soul shards
 		elseif itemID == 6265 then
-			tinsert(sortKey, 14)
+			tinsert(sortKey, 13)
 
 		-- conjured items
 		elseif conjured then
-			tinsert(sortKey, 15)
+			tinsert(sortKey, 14)
 
 		-- soulbound items
 		elseif soulbound then
@@ -508,29 +476,21 @@ function Item(container, position)
 		elseif usable and classId ~= 1 and classId ~= 2 and classId ~= 8 or classId == 4 then
 			tinsert(sortKey, 8)
 
-		-- enchanting materials
-		elseif ENCHANTING_MATERIALS[itemID] then
-			tinsert(sortKey, 11)
-
-		-- herbs
-		elseif HERBS[itemID] then
-			tinsert(sortKey, 12)
-
 		-- higher quality
 		elseif quality > 1 then
 			tinsert(sortKey, 10)
 
 		-- common quality
 		elseif quality == 1 then
-			tinsert(sortKey, 13)
+			tinsert(sortKey, 11)
 			tinsert(sortKey, -sellPrice)
 
 		-- junk
 		elseif quality == 0 then
-			tinsert(sortKey, 14)
+			tinsert(sortKey, 12)
 			tinsert(sortKey, sellPrice)
 		end
-		
+
 		tinsert(sortKey, classId)
 		tinsert(sortKey, slot)
 		tinsert(sortKey, subClassId)
